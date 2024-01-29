a56ebf14dc09d23156e4f30cbe57dd4eb3c635bc
Camille Ko <48820497+camillekokoko@users.noreply.github.com>
2024-01-29 11:55:32 +1030

    Update Strings.cs

--- Strings.cs
+++ Strings.cs
@@ -7,7 +7,7 @@ namespace PrideAndPrejudice
     static void Main(string[] args)
     {
       // First string variable
-      string firstSentence = "It is a truth universally acknowledged, that a single man in possession of a good foartune, must be in want of a wife.";
+      string firstSentence = "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.";
 
       // Second string variable
       string firstSpeech = "\"My dear Mr. Bennet,\" said his lady to him one day, \"have you heard that Netherfield Park is let at last?\"";
